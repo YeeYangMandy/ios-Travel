06870fa7377778d0631736922f14f3bd8c569915
EB209 <eb209@EB209de-MacBook-Pro-9.local>
2025-12-28 14:28:54 +0800

    Add code

--- Travel/ChecklistViewController.swift
+++ Travel/ChecklistViewController.swift
@@ -1,29 +1,95 @@
-//
-//  ChecklistViewController.swift
-//  Travel
-//
-//  Created by EB209 on 2025/12/25.
-//
-
 import UIKit
 
-class ChecklistViewController: UIViewController {
+// 自定義 Cell 類別 (記得在 Storyboard 的 Identity Inspector 綁定這個 Class)
+class ChecklistCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
+    var toggleAction: (() -> Void)? // 閉包回調
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        toggleAction?()
+    }
+}
 
+class ChecklistViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var items: [PackingItem] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        loadItems()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Data Persistence
+    func loadItems() {
+        if let data = UserDefaults.standard.data(forKey: "SavedList"),
+           let saved = try? JSONDecoder().decode([PackingItem].self, from: data) {
+            items = saved
+        } else {
+            // 預設清單
+            items = [
+                PackingItem(title: "護照", isChecked: false),
+                PackingItem(title: "錢包 (含信用卡)", isChecked: false),
+                PackingItem(title: "牙刷/盥洗用具", isChecked: false),
+                PackingItem(title: "充電器/轉接頭", isChecked: false)
+            ]
+        }
     }
-    */
+    
+    func saveItems() {
+        if let data = try? JSONEncoder().encode(items) {
+            UserDefaults.standard.set(data, forKey: "SavedList")
+        }
+    }
 
+    // MARK: - TableView
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 強制轉型為自定義 Cell
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ChecklistCell", for: indexPath) as? ChecklistCell else {
+            return UITableViewCell()
+        }
+        
+        let item = items[indexPath.row]
+        cell.titleLabel.text = item.title
+        
+        // 設定勾選圖示 (需使用 SF Symbols: square / checkmark.square.fill)
+        let iconName = item.isChecked ? "checkmark.square.fill" : "square"
+        cell.checkButton.setImage(UIImage(systemName: iconName), for: .normal)
+        
+        // 處理按鈕點擊
+        cell.toggleAction = { [weak self] in
+            self?.items[indexPath.row].isChecked.toggle()
+            self?.saveItems()
+            self?.tableView.reloadRows(at: [indexPath], with: .none)
+        }
+        
+        return cell
+    }
+    
+    // MARK: - Add Item (連線到 Bar Button Item)
+    @IBAction func addItemTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "新增物品", message: nil, preferredStyle: .alert)
+        alert.addTextField { $0.placeholder = "物品名稱" }
+        
+        let add = UIAlertAction(title: "新增", style: .default) { _ in
+            if let text = alert.textFields?.first?.text, !text.isEmpty {
+                self.items.append(PackingItem(title: text, isChecked: false))
+                self.saveItems()
+                self.tableView.reloadData()
+            }
+        }
+        
+        alert.addAction(add)
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel))
+        present(alert, animated: true)
+    }
 }
--- Travel/ItineraryViewController.swift
+++ Travel/ItineraryViewController.swift
@@ -1,29 +1,57 @@
-//
-//  ItineraryViewController.swift
-//  Travel
-//
-//  Created by EB209 on 2025/12/25.
-//
-
 import UIKit
 
-class ItineraryViewController: UIViewController {
+class ItineraryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    // 假資料
+    var events: [TripEvent] = [
+        TripEvent(time: "09:00", title: "成田機場落地", location: "Terminal 2"),
+        TripEvent(time: "11:30", title: "飯店寄放行李", location: "上野站前 APA"),
+        TripEvent(time: "13:00", title: "淺草寺參拜", location: "淺草")
+    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - TableView Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return events.count
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TripCell", for: indexPath)
+        let event = events[indexPath.row]
+        
+        // 需設定 Cell Style 為 Subtitle
+        cell.textLabel?.text = "\(event.time) - \(event.title)"
+        cell.detailTextLabel?.text = "📍 \(event.location)"
+        return cell
+    }
+    
+    // MARK: - Add Action (連線到 Bar Button Item)
+    @IBAction func addEventTapped(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "新增行程", message: nil, preferredStyle: .alert)
+        alert.addTextField { $0.placeholder = "時間 (09:00)" }
+        alert.addTextField { $0.placeholder = "活動名稱" }
+        alert.addTextField { $0.placeholder = "地點" }
+        
+        let addAction = UIAlertAction(title: "加入", style: .default) { _ in
+            guard let time = alert.textFields?[0].text,
+                  let title = alert.textFields?[1].text,
+                  let loc = alert.textFields?[2].text else { return }
+            
+            // 如果是做共同行程，這段要改成寫入 Firebase
+            self.events.append(TripEvent(time: time, title: title, location: loc))
+            self.events.sort { $0.time < $1.time } // 簡單排序
+            self.tableView.reloadData()
+        }
+        
+        alert.addAction(addAction)
+        alert.addAction(UIAlertAction(title: "取消", style: .cancel))
+        present(alert, animated: true)
+    }
 }
